849f94ff07b405eab96655bce07b4b4c0f87116a
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-09-24 12:17:51 -0300

    refactor: create ConfigSection SFC

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts">
+import ConfigSection from './components/ConfigSection.vue'
 import QRCodeSection from './components/QRCodeSection.vue'
-import { qrcodeValue } from './lib/qrcodeValue'
 </script>
 
 <template>
   <div class="flex items-center justify-center h-screen">
     <div class="flex w-2/3 gap-5 h-2/3">
-      <div class="flex items-center justify-center w-2/3 h-full bg-white shadow-lg rounded-xl">
-        <textarea
-          v-model="qrcodeValue"
-          class="w-full bg-transparent resize-none h-44 focus:ring-0"
-          placeholder="Write your text here..."
-        />
-      </div>
+      <ConfigSection />
       <QRCodeSection />
     </div>
   </div>
---
+++ src/components/ConfigSection.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { qrcodeValue } from '../lib/qrcodeValue'
+</script>
+<template>
+  <div class="flex items-center justify-center w-2/3 h-full bg-white shadow-lg rounded-xl">
+    <textarea
+      v-model="qrcodeValue"
+      class="w-full bg-transparent resize-none h-44 focus:ring-0"
+      placeholder="Write your text here..."
+    />
+  </div>
+</template>
