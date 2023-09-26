8bc307c7b0228f4a624b8387a3f88fdf1581f7dc
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-09-25 21:34:10 -0300

    refactor: create PlainText component

---
+++ src/components/PlainText.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { qrcodeValue } from '../lib/qrcodeValue'
+</script>
+
+<template>
+  <textarea
+    v-model="qrcodeValue"
+    class="w-full text-xl resize-none p-4 rounded-md flex-1 bg-white/30 transition-colors leading-none border-none focus:ring-blue-800/20"
+    placeholder="Write your text here..."
+  />
+</template>
