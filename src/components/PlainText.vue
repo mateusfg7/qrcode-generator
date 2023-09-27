ea94c921cd3355a171b9156880a2d6a32847d21a
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-09-27 20:17:53 -0300

    feat(ui): reset plain text component content when mounted

--- src/components/PlainText.vue
+++ src/components/PlainText.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue'
 import { qrcodeValue } from '../lib/qrcodeValue'
+
+const currValue = ref('')
+
+watch([currValue], () => {
+  qrcodeValue.value = currValue.value
+})
 </script>
 
 <template>
   <textarea
-    v-model="qrcodeValue"
+    v-model="currValue"
     class="w-full text-xl resize-none p-4 rounded-md flex-1 bg-white/30 transition-colors leading-none border-none focus:ring-blue-800/20"
     placeholder="Write your text here..."
   />
