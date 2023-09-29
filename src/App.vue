9264c75595b1d98f826261166e21890d6539a876
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-09-28 22:35:16 -0300

    feat(ui): add author information

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ArrowUpRight } from 'lucide-vue-next'
+
 import ConfigSection from './components/ConfigSection.vue'
 import QRCodeSection from './components/QRCodeSection.vue'
 </script>
@@ -11,6 +13,13 @@ import QRCodeSection from './components/QRCodeSection.vue'
     <div
       class="bg-blue-600/20 h-[500px] w-[500px] -z-20 rounded-full absolute top-[300px] left-[700px] blur-[100px]"
     />
+    <a
+      href="https://mateusf.com"
+      rel="author"
+      target="_blank"
+      class="inline-flex absolute bottom-3 right-3 text-sm leading-none items-end opacity-30 hover:opacity-100 transition-opacity"
+      >Author <ArrowUpRight class="w-2 h-min"
+    /></a>
     <div class="flex w-[70%] gap-5 h-fit">
       <ConfigSection />
       <QRCodeSection />
